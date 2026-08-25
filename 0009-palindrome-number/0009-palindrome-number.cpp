cdb85635ef9bb60fe8b48c0611a311ceb1aab714
chauhan-prerana <cprerana2104@gmail.com>
2026-08-25 21:14:19 +0530

    Time: 4 ms (30.43%), Space: 8.6 MB (64.88%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0))
-            return false;
-
-        int reversed = 0;
-
-        while (x > reversed) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
+        long long revnum = 0 ;
+        int dup = x;
+        while(x>0){
+            int ld = x%10;
+            revnum = (revnum * 10)+ld;
+            x = x/10;
         }
-
-        return x == reversed || x == reversed / 10;
+        
+        return dup == revnum;
     }
 };
